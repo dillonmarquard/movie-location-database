c3593c96a28ab0d9a491816c7eeabe660f927355
Dillon Marquard <dmarquard4@gmail.com>
2022-11-02 16:54:51 -0700

    sql to implement

---
+++ sandbox_query/Q1.sql
@@ -0,0 +1,9 @@
+-- main queries
+    -- add movie to database
+    -- add location
+    -- edit location
+    -- delete location
+    -- view collection by (genre, location, actors, writers)
+
+-- subqueries
+    -- get table (locations, genres, actors, writers)
