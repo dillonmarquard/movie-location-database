accff2558edbc1e02f00e9bf5b794806c92769ac
Dillon Marquard <dmarquard4@gmail.com>
2022-11-02 20:32:17 -0700

    loads schema into db

--- interface.py
+++ interface.py
@@ -1,6 +1,31 @@
+import sqlite3
+
 class interface:
-    def __init__(self,_conn):
-        self._conn = _conn
+    def __init__(self):
+        self.db = sqlite3.connect("movie_db.sqlite")
+        self._cur = self.db.cursor()
+
+    def load_schema(self):
         
+        try:
+            with open("sandbox_schema/db_schema.sql", 'r') as sql_file:
+                sql = sql_file.read()
+            tmp = self._cur.executescript(sql)
+        except:
+            print("ERROR: load_schema")
+        
+    def load_data(self):
+        pass
+
     def add_movie(self,_name,_studio,_year):
         pass
+
+    def parse_output(self):
+        pass 
+
+def main():
+    _fct = interface()
+    _fct.load_schema()
+
+if __name__ == "__main__":
+    main()
--- sandbox_schema/db_schema.sql
+++ sandbox_schema/db_schema.sql
@@ -1,29 +1,28 @@
-create table Movie (
+create table if not exists Movie (
     id TEXT primary key not null, -- from imdb
     studio_id INTEGER not null,
     title TEXT not null,
     year INTEGER not null
 );
 
-create table Studios (
+create table if not exists Studios (
     id INTEGER primary key not null,
     name TEXT not null
 );
 
-create table Locations (
-    id INTEGER  not null,
+create table if not exists Locations (
+    id INTEGER primary key not null,
     address TEXT unique not null,
-    info TEXT not null,
-    primary_key(id,address)
+    info TEXT not null
 );
 
-create table Genres (
+create table if not exists Genres (
     id INTEGER not null,
     name TEXT unique not null,
-    primary_key(id,name)
+    primary key (id,name)
 );
 
-create table Actors (
+create table if not exists Actors (
     id INTEGER primary key not null,
     firstname TEXT not null,
     lastname TEXT not null,
@@ -31,31 +30,31 @@ create table Actors (
     died DATE
 );
 
-create table Directors (
+create table if not exists Directors (
     id INTEGER primary key not null,
     firstname TEXT not null,
-    lastname TEXT not null,
+    lastname TEXT not null
 );
 
-create table MovieGenre (
+create table if not exists MovieGenre (
     movie_id TEXT not null,
     genre_id INTEGER not null,
-    primary_key(movie_id,genre_id)
+    primary key (movie_id,genre_id)
 );
 
-create table MovieLocation (
+create table if not exists MovieLocation (
     movie_id TEXT not null,
     location_id INTEGER not null
 );
 
-create table MovieActor (
+create table if not exists MovieActor (
     movie_id TEXT not null,
     actor_id INTEGER not NULL,
-    primary_key(movie_id,actor_id)
+    primary key (movie_id,actor_id)
 );
 
-create table MovieDirector (
+create table if not exists MovieDirector (
     movie_id TEXT not null,
     director_id INTEGER not null,
-    primary_key(movie_id,director_id)
+    primary key (movie_id,director_id)
 );
